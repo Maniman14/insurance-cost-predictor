ba5ea70299655df9a10bccb7284021833637a104
Maniman14 <mananjain2006@gmail.com>
2026-07-28 14:38:20 +0530

    insurance cost predictor

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+model = joblib.load('model.pkl')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    age = float(request.form['age'])
+    bmi = float(request.form['bmi'])
+    children = int(request.form['children'])
+    smoker = 1 if request.form['smoker'] == 'yes' else 0
+    sex = 1 if request.form['sex'] == 'male' else 0
+    region = int(request.form['region'])  
+    features = np.array([[age, bmi, children, smoker, sex, region]])
+    prediction = model.predict(features)[0]
+    return render_template('index.html', prediction_text=f'Estimated Cost: ${prediction:.2f}')
+
+if __name__ == '__main__':
+    app.run(debug=True)
